501c63a67d38914e89379e1e6b0803a12c4aa742
ScottB <17sb@cis.dk>
2017-12-13 21:49:06 +0100

    Add config.api_only line to application.rb request spec for performancedata passing

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ Bundler.require(*Rails.groups)
 
 module CooperApi
   class Application < Rails::Application
+    config.api_only = true
     # [...]
     config.middleware.insert_before 0, Rack::Cors do
       allow do
