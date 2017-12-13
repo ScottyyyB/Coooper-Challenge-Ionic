c0014c1da373eb9fa78e767d52a9e7fdbef3a195
ScottB <17sb@cis.dk>
2017-12-13 21:22:04 +0100

    PerformanceData controller implmentation near finish

--- spec/requests/api/v1/performance_data_spec.rb
+++ spec/requests/api/v1/performance_data_spec.rb
@@ -1,10 +1,12 @@
 RSpec.describe Api::V1::PerformanceDataController, type: :request do
-  let(:headers) { { HTTP_ACCEPT: 'application/json'} }
+  let(:user) { FactoryBot.create(:user) }
+  let(:credentials) { user.create_new_auth_token }
+  let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
 
   describe 'POST /api/v1/performance_data' do
     it 'creates a data entry' do
       post '/api/v1/performance_data', params: {
-        performance_data: { data: { message: 'Average'} }
+        performance_data: { data: { message: 'Average' } }
       }, headers: headers
 
       entry = PerformanceData.last
