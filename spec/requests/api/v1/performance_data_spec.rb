ff1cdb509a5d27654073bf350406548dc4ced3d2
ScottB <17sb@cis.dk>
2017-12-14 10:16:39 +0100

    Finish specs for performance controller and get route for performance_data

--- spec/requests/api/v1/performance_data_spec.rb
+++ spec/requests/api/v1/performance_data_spec.rb
@@ -13,4 +13,22 @@ RSpec.describe Api::V1::PerformanceDataController, type: :request do
       expect(entry.data).to eq 'message' => 'Average'
     end
   end
+
+  describe 'GET /api/v1/performance_data' do
+    before do
+      5.times { user.performance_data.create(data: { message: 'Average'}) }
+    end
+
+    it 'returns a collection of performance data' do
+      get '/api/v1/performance_data', headers: headers
+      expect(response_json['entries'].count).to eq 5
+    end
+
+    it 'returns a collection of performance data for the correct user' do
+      get '/api/v1/performance_data', headers: headers
+      user.performance_data.each do |performance_data|
+        expect(performance_data.data['message']).to eq 'Average'
+      end
+    end
+  end
 end
